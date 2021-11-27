2a3bc802ebe389712e0be8d3f32e5055a05b06b0
Min-Zheng Shieh <mzshieh@nctu.edu.tw>
2021-11-27 08:29:44 +0800

    %(%F_%H%M%S)T

--- update.sh
+++ update.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-curl http://localhost/domjudge/public?static=1 --output index.html
-git add .
-git commit -m "%(%F_%H%M%S)T"
-git push
+counter=$(( $1 * 2 ))
+while [ $counter -gt 0 ]
+do
+	curl http://localhost/domjudge/public?static=1 --output index.html
+	git add .
+	git commit -m "%(%F_%H%M%S)T"
+	git push
+	counter=$(( $counter - 1 ))
+done
