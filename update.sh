11da4fd746aedb64c06e93eab3bd1a4dee0262d6
Min-Zheng Shieh <mzshieh@nctu.edu.tw>
2021-11-27 08:54:10 +0800

    Sat 27 Nov 2021 08:54:10 AM CST

--- update.sh
+++ update.sh
@@ -4,7 +4,7 @@ while [ $counter -gt 0 ]
 do
 	curl http://localhost/domjudge/public?static=1 --output index.html
 	git add .
-	git commit -m "%(%F_%H%M%S)T"
+	git commit -m "$(date)"
 	git push
 	sleep 30
 	counter=$(( $counter - 1 ))
